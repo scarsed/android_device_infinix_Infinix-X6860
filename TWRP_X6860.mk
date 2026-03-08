d2b3ec96a10664151ecec45ed1e759a27dbf4ce6
scarsed <kimaniwilber@gmail.com>
2026-03-08 22:02:37 +0300

    Update copyright year and product specifics in TWRP file

--- TWRP_X6860.mk
+++ TWRP_X6860.mk
@@ -1,29 +1,21 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from TECNO-CLA6 device
+$(call inherit-product, device/infinix/X6860/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from Infinix-X6860 device
-$(call inherit-product, device/infinix/Infinix-X6860/device.mk)
-
-PRODUCT_DEVICE := Infinix-X6860
-PRODUCT_NAME := omni_Infinix-X6860
+# Product Specifics
+PRODUCT_NAME := twrp_x6860
+PRODUCT_DEVICE := X6860
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6860
-PRODUCT_MANUFACTURER := infinix
+PRODUCT_MANUFACTURER := INFINIX
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vext_x6860_h899-user 12 SP1A.210812.016 934438 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X6860-OP/Infinix-X6860:12/SP1A.210812.016/250806V366:user/release-keys
